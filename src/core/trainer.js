ec90b55c433df4f7d936710dca2d6733d16e3bfb
wwebtvmedia <wwebtvmedia@gmail.com>
2026-04-17 19:49:02 -0400

    fix: add missing CONFIG import in base trainer

--- src/core/trainer.js
+++ src/core/trainer.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { ModelManager } from "./models.js";
+import { CONFIG } from "../config.js";
 
 class SwarmTrainer {
   constructor(network, phaseManager) {
